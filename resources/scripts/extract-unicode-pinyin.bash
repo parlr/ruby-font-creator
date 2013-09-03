ff8ad4b39b81ee279378714cce6e1772eccaa684
Edouard Lopez <dev+git@edouard-lopez.com>
2013-09-03 14:34:48 +0200

    feat(script): add extractor script for unicode/pinyin

---
+++ resources/scripts/extract-unicode-pinyin.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# DESCRIPTION
+#   Extract unicode and pinyin prononciation and generate the character
+#
+# USAGE
+#   bash ./scripts/extract-unicode-pinyin.bash
+#
+# @author: Édouard Lopez <dev+hpf@edouard-lopez.com>
+
+inputFile="${1:-./unihan/Unihan/Unihan_Readings.txt}"
+outputFile="${2:-./unihan/unicode-pinyin.csv}"
+
+rm  "$outputFile"
+
+
+while read -r unicode field pinyin;
+do
+  if [[ $unicode = U+* && $field = kMandarin ]]; then
+    printf '%s;%b;%s\n' \
+      "$unicode" \
+      "\\u${unicode#U+}" \
+      "$pinyin" \
+    >> "$outputFile"
+  fi
+done < "$inputFile"
