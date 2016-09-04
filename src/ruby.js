e153fb3fda3b19ed7a1a71c00107e1ee5d631367
Édouard Lopez <dev+git@edouard-lopez.com>
2016-09-05 00:18:25 +0200

    use jsdom to parse SVG

--- src/ruby.js
+++ src/ruby.js
@@ -1,18 +1,23 @@
 import TextToSVG from 'text-to-svg';
+import jsdom from 'jsdom';
 
 const textToSVG = TextToSVG.loadSync();
 
 export default {
+	rightDownward: 'matrix(0,1,-1,0,0,0)',
+	rightUpward: 'matrix(0,-1,1,0,0,0)',
 	text(chinese = '汉字') {
 		const attrs = {
 			fill: 'black',
-			stroke: 'black'};
+			stroke: 'black'
+		};
 		const options = {
 			x: 0,
 			y: 0,
 			fontSize: 72,
 			anchor: 'top',
-			attributes: attrs};
+			attributes: attrs
+		};
 
 		return textToSVG.getSVG(chinese, options);
 	},
@@ -20,19 +25,20 @@ export default {
 		const attrs = {
 			fill: 'red',
 			stroke: 'red',
-			transform: 'matrix(0,1,-1,0,0,0)'};
+			transform: 'matrix(0,1,-1,0,0,0)'
+		};
 		const options = {
 			x: 0,
 			y: 0,
 			fontSize: 32,
 			anchor: 'top',
-			attributes: attrs};
+			attributes: attrs
+		};
 
 		return textToSVG.getSVG(pinyin, options);
 	},
 	getData(doc) {
-		const parser = new DOMParser();
-		const svg = parser.parseFromString(doc, 'image/svg+xml');
+		const svg = jsdom.jsdom(doc);
 		const path = svg.querySelector('path');
 
 		return path.attributes.getNamedItem('d').value;
