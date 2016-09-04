b9fe39d9db797573e24681af62b39bdac90b6ade
Édouard Lopez <dev+git@edouard-lopez.com>
2016-09-04 21:42:40 +0200

    fix xo issue

--- src/ruby.js
+++ src/ruby.js
@@ -4,14 +4,29 @@ const textToSVG = TextToSVG.loadSync();
 
 export default {
 	text(chinese = '汉字') {
-		const attributes = {fill: 'black', stroke: 'black'};
-		const options = {x: 0, y: 0, fontSize: 72, anchor: 'top', attributes: attributes};
+		const attrs = {
+			fill: 'black',
+			stroke: 'black'};
+		const options = {
+			x: 0,
+			y: 0,
+			fontSize: 72,
+			anchor: 'top',
+			attributes: attrs};
 
 		return textToSVG.getSVG(chinese, options);
 	},
 	annotation(pinyin = 'hanzi') {
-		const attributes = {fill: 'red', stroke: 'red', transform: 'matrix(0,1,-1,0,0,0)'};
-		const options = {x: 0, y: 0, fontSize: 32, anchor: 'top', attributes: attributes};
+		const attrs = {
+			fill: 'red',
+			stroke: 'red',
+			transform: 'matrix(0,1,-1,0,0,0)'};
+		const options = {
+			x: 0,
+			y: 0,
+			fontSize: 32,
+			anchor: 'top',
+			attributes: attrs};
 
 		return textToSVG.getSVG(pinyin, options);
 	}
