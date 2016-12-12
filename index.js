a59383129b2cfd6b984908f1e9208dee4d65666c
Édouard Lopez <dev+git@edouard-lopez.com>
2016-12-12 01:04:54 +0100

    create 'build' directory if need be

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import ruby from './src/ruby';
 import svg from './src/svg';
 
+const fs = require('fs');
+
+fs.mkdir('./build', 0o700, (err) => {
+  if (err) console.log('already exists');
+});
+
 const glyph = ruby.text('北京');
 svg.save('./build/glyph.svg', glyph);
 
