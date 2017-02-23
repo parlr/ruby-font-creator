37835988064436ae8b73835acab827f914309753
hugolpz <hugo.lpz@gmail.com>
2017-02-23 12:22:57 +0100

    add for loop, working :D

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ const textToSVGpho = TextToSVG.loadSync('./fonts/FreeSansBold.ttf'); // local cu
 fs.mkdir('./build', 0o700, err => { if (err) { console.log('./build folder already exists'); } });
 
 // Variables values
-var 西 = { ort: '西', pho: 'xī', style: 'top' };
-var 中 = { ort: '中', pho: 'zhong1', style: 'top' };
-var sinogram = 西;
+var list = [
+	{ ort: '西', pho: 'xī', style: 'top' },
+	{ ort: '中', pho: 'zhong1', style: 'top' }
+];
 
 // All possibles styles
 const styles= {
@@ -32,19 +33,21 @@ const styles= {
 	"right-upward" : { }
 };
 
-// Active style
-var style = styles[sinogram.style];
-
-const svgOrt = textToSVGort.getPath(sinogram.ort, style.ort); // create ortho's path
-const svgPho = textToSVGpho.getPath(sinogram.pho, style.pho); // create phono's path
-
-var svg = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="80" height="80" style="border:1px solid #666">`
-    + svgOrt
-    + svgPho
-    + `</svg>`;
-
-fs.writeFile('./build/'+ sinogram.ort +'.svg', svg);
-
-console.log('File generation : done.');
-console.log('File data : '+JSON.stringify(sinogram));
-console.log('File location : ./build/'+sinogram.ort+'.svg');
+for (let char of list) {
+	// Active style
+	var style = styles[char.style];
+	// Create svg, then paths
+	const svgOrt = textToSVGort.getPath(char.ort, style.ort);
+	const svgPho = textToSVGpho.getPath(char.pho, style.pho);
+	//Create valid svg file's data
+	var svg = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="80" height="80" style="border:1px solid #666">`
+	    + svgOrt
+	    + svgPho
+	    + `</svg>`;
+	// Print to file
+	fs.writeFile('./build/'+ char.ort +'.svg', svg);
+	// Few feedbacks
+	console.log('File generation : done.');
+	console.log('File data : '+JSON.stringify(char));
+	console.log('File location : ./build/'+char.ort+'.svg');
+}
