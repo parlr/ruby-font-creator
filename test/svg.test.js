69ee50b148fcdc1868bb44d0e383b3877e301cd5
Édouard Lopez <dev+git@edouard-lopez.com>
2017-02-25 00:05:18 +0100

    output test to unique file

--- test/svg.test.js
+++ test/svg.test.js
@@ -1,12 +1,16 @@
+import fs from 'fs';
 import test from 'ava';
+import del from 'del';
 import svg from '../src/svg';
 
-const fs = require('fs');
+test.afterEach.always(() => {
+	del(['./test/test*.svg']);
+});
 
 test('should save data to svg file asynchornously', async t => {
 	const content = '<svg xmlns="http://www.w3.org/2000/svg"><path d="M22.64 50.17Q15.01…"/></svg>';
 
-	const filename = './test/test.svg';
+	const filename = './test/test1.svg';
 	await svg.save(filename, content);
 
 	t.true(fs.statSync(filename).size > 0);
@@ -15,7 +19,7 @@ test('should save data to svg file asynchornously', async t => {
 test('should save data to svg file', t => {
 	const content = '<svg xmlns="http://www.w3.org/2000/svg"><path d="M22.64 50.17Q15.01…"/></svg>';
 
-	const filename = './test/test.svg';
+	const filename = './test/test2.svg';
 	svg.saveSync(filename, content);
 
 	t.true(fs.statSync(filename).size > 0);
