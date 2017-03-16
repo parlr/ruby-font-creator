25a504d5f4d4a1f622b55511cd7485902c6f8205
Édouard Lopez <contact@edouard-lopez.com>
2017-03-16 02:08:07 +0100

    extract list of glyphs to data.json
    see #13

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-import fs from 'fs';
-import ruby from './src/ruby';
-import svg from './src/svg';
-import layout from './src/layouts';
+import fs from "fs";
+import jsonfile from "jsonfile";
+import ruby from "./src/ruby";
+import svg from "./src/svg";
+import layout from "./src/layouts";
 
-const data = [
-	{glyph: '西', phonetic: 'xī'},
-	{glyph: '中', phonetic: 'zong1'}
-];
+start();
 
-prepare();
-build(data);
+function start(filepath = "src/data.json") {
+  jsonfile.readFile(filepath, function(err, data) {
+    if (err) {
+      throw err;
+    }
+    prepare();
+    build(data);
+  });
+}
 
 function build(data) {
-	const options = {width: 80, height: 80};
-	for (let datum = 0; datum < data.length; datum++) {
-		const char = data[datum];
-		const svgContent = svg.wrap([
-			ruby.text(char.glyph, layout.bottom.glyph(options)),
-			ruby.annotation(char.phonetic, layout.top.phonetic(options))
-		]);
-		svg.save(`./build/${char.glyph}.svg`, svgContent);
-	}
+  const options = { width: 80, height: 80 };
+  for (let datum = 0; datum < data.length; datum++) {
+    const char = data[datum];
+    const svgContent = svg.wrap([
+      ruby.text(char.glyph, layout.bottom.glyph(options)),
+      ruby.annotation(char.phonetic, layout.top.phonetic(options))
+    ]);
+    svg.save(`./build/${char.glyph}.svg`, svgContent);
+  }
 }
 
 function prepare() {
-	fs.mkdir('./build', 0o700, err => {
-		if (err) {
-			console.log('already exists');
-		}
-	});
+  fs.mkdir("./build", 0o700, err => {
+    if (err) {
+      console.log("already exists");
+    }
+  });
 }
