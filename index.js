a0ae041d386a6d9c4106e933a13d81403a6cfa04
Édouard Lopez <contact@edouard-lopez.com>
2017-03-27 01:04:46 +0200

    generate font files (svg, ttf, eot, woff, woff2)
    closes #12

--- index.js
+++ index.js
@@ -1,37 +1,85 @@
 import fs from "fs";
+import webfont from "webfont";
+import path from "path";
 import jsonfile from "jsonfile";
 import ruby from "./src/ruby";
 import svg from "./src/svg";
 import layout from "./src/layouts";
 
-start();
-
-function start(filepath = "src/data.json") {
-  jsonfile.readFile(filepath, function(err, data) {
-    if (err) {
-      throw err;
-    }
-    prepare();
-    build(data);
-  });
-}
-
-function build(data) {
-  const options = { width: 80, height: 80 };
-  for (let datum = 0; datum < data.length; datum++) {
-    const char = data[datum];
-    const svgContent = svg.wrap([
-      ruby.text(char.glyph, layout.bottom.glyph(options)),
-      ruby.annotation(char.phonetic, layout.top.phonetic(options))
-    ]);
-    svg.save(`./build/${char.glyph}.svg`, svgContent);
-  }
-}
-
-function prepare() {
-  fs.mkdir("./build", 0o700, err => {
+function prepare(config) {
+  fs.mkdir(config.workingDir, 0o700, err => {
     if (err) {
       console.log("already exists");
     }
   });
 }
+
+function generateSvg(data, config) {
+  const options = { width: 80, height: 80 };
+  for (let datum = 0; datum < data.length; datum += 1) {
+    const char = data[datum];
+    const svgContent = svg.wrap([
+      ruby.text(char.glyph, layout.bottom.glyph(options)),
+      ruby.annotation(char.phonetic, layout.top.phonetic(options))
+    ]);
+    svg.save(`${config.workingDir}/${char.glyph}.svg`, svgContent);
+  }
+}
+
+function buildFont(config) {
+  return webfont({
+    files: config.inputFiles,
+    fontName: config.fontName
+  })
+    .then(content => content)
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+function writeFont(content, destination) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(destination, content, error => {
+      if (error) {
+        return reject(new Error(error));
+      }
+
+      return resolve();
+    });
+  });
+}
+
+function generateFontFiles(content, config) {
+  // config.formats = ['svg', 'ttf', 'eot', 'woff', 'woff2'];
+  config.formats.map(format => {
+    writeFont(content[format], `${config.destFilename}.${format}`)
+      .then(() => {
+        console.log(`wrote: ${config.destFilename}.${format}`);
+      })
+      .catch(err => {
+        console.log(`failed to write ${config.destFilename}.${format}`, err);
+      });
+  });
+}
+
+function start(filepath = "src/data.json") {
+  jsonfile.readFile(filepath, (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    const config = {
+      inputFiles: "./build/**/*.svg",
+      workingDir: "./build",
+      fontName: "hpf",
+      destFilename: path.resolve("./build/hpf"),
+      formats: ["svg", "ttf", "eot", "woff", "woff2"]
+    };
+
+    prepare(config);
+    generateSvg(data, config);
+    buildFont(config).then(fontData => generateFontFiles(fontData, config));
+  });
+}
+
+start();
