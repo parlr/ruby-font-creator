9b7bd1c607f97c10c9b781e04a39404caac65873
Édouard Lopez <contact@edouard-lopez.com>
2017-03-27 16:54:43 +0200

    prettier and eslint

--- src/ruby.js
+++ src/ruby.js
@@ -1,21 +1,21 @@
-import TextToSVG from 'text-to-svg';
-import jsdom from 'jsdom';
+import TextToSVG from "text-to-svg";
+import jsdom from "jsdom";
 
 const textToSVG = TextToSVG.loadSync();
 
 export default {
-	rightDownward: 'matrix(0,1,-1,0,0,0)',
-	rightUpward: 'matrix(0,-1,1,0,0,0)',
-	text(chinese = '汉字', options) {
-		return textToSVG.getPath(chinese, options);
-	},
-	annotation(pinyin = 'hanzi', options) {
-		return textToSVG.getPath(pinyin, options);
-	},
-	getData(doc) {
-		const svg = jsdom.jsdom(doc);
-		const path = svg.querySelector('path');
+  rightDownward: "matrix(0,1,-1,0,0,0)",
+  rightUpward: "matrix(0,-1,1,0,0,0)",
+  text(chinese = "汉字", options) {
+    return textToSVG.getPath(chinese, options);
+  },
+  annotation(pinyin = "hanzi", options) {
+    return textToSVG.getPath(pinyin, options);
+  },
+  getData(doc) {
+    const svg = jsdom.jsdom(doc);
+    const path = svg.querySelector("path");
 
-		return path.attributes.getNamedItem('d').value;
-	}
+    return path.attributes.getNamedItem("d").value;
+  }
 };
--- test/ruby.test.js
+++ test/ruby.test.js
@@ -1,29 +1,29 @@
-import test from 'ava';
+import test from "ava";
 
-import ruby from '../src/ruby';
+import ruby from "../src/ruby";
 
-test('should extract path data', t => {
-	const doc = '<svg xmlns="http://www.w3.org/2000/svg"><path d="M22.64 50.17Q15.01…"/></svg>';
+test("should extract path data", t => {
+  const doc = '<svg xmlns="http://www.w3.org/2000/svg"><path d="M22.64 50.17Q15.01…"/></svg>';
 
-	const data = ruby.getData(doc);
+  const data = ruby.getData(doc);
 
-	t.is(data, 'M22.64 50.17Q15.01…');
+  t.is(data, "M22.64 50.17Q15.01…");
 });
 
-test('should create svg <path> with text', t => {
-	const chinese = '北';
+test("should create svg <path> with text", t => {
+  const chinese = "北";
 
-	const doc = ruby.text(chinese);
-	const data = ruby.getData(doc);
+  const doc = ruby.text(chinese);
+  const data = ruby.getData(doc);
 
-	t.is(data.length > 0, true);
+  t.is(data.length > 0, true);
 });
 
-test('should create svg <path> with annotation', t => {
-	const annotation = 'běi';
+test("should create svg <path> with annotation", t => {
+  const annotation = "běi";
 
-	const doc = ruby.annotation(annotation);
-	const data = ruby.getData(doc);
+  const doc = ruby.annotation(annotation);
+  const data = ruby.getData(doc);
 
-	t.is(data.length > 0, true);
+  t.is(data.length > 0, true);
 });
