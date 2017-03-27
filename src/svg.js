8a0625c148d9e3baadb0d9846900ed3962703b07
Édouard Lopez <contact@edouard-lopez.com>
2017-03-27 16:54:21 +0200

    use ES6

--- src/svg.js
+++ src/svg.js
@@ -1,15 +1,13 @@
-const fs = require('fs');
+import fs from "fs";
 
 export default {
-	saveSync: (filename, content) => {
-		fs.writeFileSync(filename, content);
-	},
-	save: (filename, content) => {
-		return fs.writeFileSync(filename, content);
-	},
-	wrap: (text, annotation, options = {width: 80, height: 80}) => {
-		return `<svg xmlns="http://www.w3.org/2000/svg" width="${options.width}" height="${options.height}">` +
-			text + annotation +
-			`</svg>`;
-	}
+  saveSync: (filename, content) => {
+    fs.writeFileSync(filename, content);
+  },
+  save: (filename, content) => fs.writeFileSync(filename, content),
+  wrap: (text, annotation, options = { width: 80, height: 80 }) =>
+    `<svg xmlns="http://www.w3.org/2000/svg" width="${options.width}" height="${options.height}">
+        ${text}
+        ${annotation}
+      </svg>`
 };
