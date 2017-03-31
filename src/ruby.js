63286d33fa1bff9031616124b2256591aef840db
Édouard Lopez <contact@edouard-lopez.com>
2017-03-31 23:27:26 +0200

    remove useless code

--- src/ruby.js
+++ src/ruby.js
@@ -2,14 +2,10 @@ import TextToSVG from "text-to-svg";
 import jsdom from "jsdom";
 
 export default {
-  rightDownward: "matrix(0,1,-1,0,0,0)",
-  rightUpward: "matrix(0,-1,1,0,0,0)",
-  converter: null,
   loadFont: fontFilepath => TextToSVG.loadSync(fontFilepath),
-  getBase: (converter, glyph = "汉字", options) =>
-    converter.getPath(glyph, options),
-  getAnnotation: (converter, text = "hanzi", options) =>
-    converter.getPath(text, options),
+  getBase: (engine, glyph = "汉字", options) => engine.getPath(glyph, options),
+  getAnnotation: (engine, text = "hanzi", options) =>
+    engine.getPath(text, options),
   getData: doc => {
     const svg = jsdom.jsdom(doc);
     const path = svg.querySelector("path");
