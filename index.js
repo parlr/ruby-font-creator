c2b9fbd5d6bfc586198cbe006df463aaccdce5e6
Édouard Lopez <contact@edouard-lopez.com>
2017-03-31 23:31:13 +0200

    hoist all config at the top of the module

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 import webfont from "webfont";
 import path from "path";
 import jsonfile from "jsonfile";
+
 import helpers from "./src/helpers";
 import ruby from "./src/ruby";
 import svg from "./src/svg";
 import layout from "./src/layouts";
 
+const config = {
+  dataSource: "src/data.json",
+  canvas: { width: 80, height: 80 },
+  inputFiles: "./build/**/*.svg",
+  workingDir: "./build/svg",
+  fontName: "Hanzi-Pinyin-Font",
+  destFilename: path.resolve("./build/Hanzi-Pinyin-Font"),
+  formats: ["ttf", "woff2"]
+};
+
 function generateSvg(data, config) {
-  const options = { width: 80, height: 80 };
   const converter = ruby.loadFont("./resources/fonts/NotoSansTC-Regular.otf");
 
   for (let datum = 0; datum < data.length; datum += 1) {
     const char = data[datum];
     const svgContent = svg.wrap(
-      ruby.getBase(converter, char.glyph, layout.base.bottom(options)),
-      ruby.getAnnotation(converter, char.ruby, layout.annotation.top(options))
-    );
+      ruby.getBase(converter, char.glyph, layout.base.bottom(config.canvas)),
+      ruby.getAnnotation(
+        converter,
+        char.ruby,
+        layout.annotation.top(config.canvas)
+      )
     );
     svg.save(`${config.workingDir}/${char.glyph}.svg`, svgContent);
   }
@@ -31,24 +44,17 @@ function buildFont(config) {
     .catch(err => console.log(err));
 }
 
-function start(filepath = "src/data.json") {
-  jsonfile.readFile(filepath, (err, data) => {
+function start(config) {
+  jsonfile.readFile(config.dataSource, (err, data) => {
     if (err) {
       throw err;
     }
 
-    const config = {
-      inputFiles: "./build/**/*.svg",
-      workingDir: "./build/svg",
-      fontName: "Hanzi-Pinyin-Font",
-      destFilename: path.resolve("./build/Hanzi-Pinyin-Font"),
-      formats: ["ttf", "woff2"]
-    };
-
     helpers.prepare(config);
     generateSvg(data, config);
-    buildFont(config); //.then(fontData => helpers.generateFontFiles(fontData, config));
+    buildFont(config).then(fontData =>
+      helpers.generateFontFiles(fontData, config));
   });
 }
 
-start();
+start(config);
