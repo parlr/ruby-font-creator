0d32c041dd26dfbc88cd085172253a0648e54a4b
Édouard Lopez <contact@edouard-lopez.com>
2017-04-03 00:12:17 +0200

    download DroidSansFallbackFull.ttf before running test on travis-ci

---
+++ test/fetch-fonts.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+curl --location \
+	--output ./resources/fonts/DroidSansFallbackFull.ttf \
+	https://github.com/android/platform_frameworks_base/raw/master/data/fonts/DroidSansFallbackFull.ttf
